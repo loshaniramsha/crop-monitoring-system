61f5af13c8e4492e4c6c43b614bf5247a740cc5f
loshaniramsha <loshanikottagoda@gmail.com>
2024-10-28 00:25:32 +0530

    first commit

---
+++ crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/CropMonitoringSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.crop_monitoring_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropMonitoringSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CropMonitoringSystemApplication.class, args);
+	}
+
+}
---
+++ crop-monitoring-system/src/test/java/com/example/crop_monitoring_system/CropMonitoringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crop_monitoring_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropMonitoringSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
