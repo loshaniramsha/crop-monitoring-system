fba2b20ff67609b06cd3dc5220091c481dea616c
loshaniramsha <loshanikottagoda@gmail.com>
2024-10-30 11:30:07 +0530

    Create CropDTO class implementing CropStates interface

---
+++ crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/dto/impl/CropDTO.java
@@ -0,0 +1,23 @@
+package com.example.crop_monitoring_system.dto.impl;
+
+import com.example.crop_monitoring_system.dto.CropStates;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CropDTO implements CropStates {
+
+    private String cropCode;
+    private String cropName;
+    private String scientificName;
+    private String cropImage;
+    private String category;
+    private String cropSeason;
+    private List<FieldDTO> fielList;
+
+}
