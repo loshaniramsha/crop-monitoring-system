eb2b514c65b5ddeef621cdb58800fd5de26bde33
loshaniramsha <loshanikottagoda@gmail.com>
2024-10-30 10:23:12 +0530

    Add StaffEntity with attributes, enum mappings, and relationships to Field and Vehicle

---
+++ crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/entity/impl/StaffEntity.java
@@ -0,0 +1,56 @@
+package com.example.crop_monitoring_system.entity.impl;
+
+import com.example.crop_monitoring_system.entity.Gender;
+import com.example.crop_monitoring_system.entity.Role;
+import com.example.crop_monitoring_system.entity.SuperEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "staff")
+public class StaffEntity implements SuperEntity {
+    @Id
+    private String staffId;
+
+    private String firstName;
+    private String lastName;
+    private String designation;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private Date joiningDate;
+    private Date birthDate;
+
+    private String addressLine1;
+    private String addressLine2;
+    private String addressLine3;
+    private String addressLine4;
+    private String addressLine5;
+
+    private String phoneNumber;
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @ManyToMany
+    @JoinTable(
+            name = "staff_field",
+            joinColumns = @JoinColumn(name = "staff_id"),
+            inverseJoinColumns = @JoinColumn(name = "field_id")
+    )
+    private List<FieldEntity> fields = new ArrayList<>();
+
+    @OneToMany(mappedBy = "staff", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<VehicleEntity> vehicles = new ArrayList<>();
+}
