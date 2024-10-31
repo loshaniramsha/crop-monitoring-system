aa185b8f05536a039ba7896b2d5feb9f7500de6f
loshaniramsha <loshanikottagoda@gmail.com>
2024-10-31 07:30:24 +0530

    changes package

--- crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/dto/CropStates.java
+++ crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/dto/CropStates.java
@@ -2,6 +2,6 @@ package com.example.crop_monitoring_system.dto;
 
 import java.io.Serializable;
 
-public interface CropStates extends Serializable,SuperDTO {
+public interface CropStates extends Serializable, SuperDTO {
 
 }
