574a7e983f6323b96a7d8e922181b3fa76fdf6b0
loshaniramsha <loshanikottagoda@gmail.com>
2024-10-31 12:40:27 +0530

    Define CropService interface with CRUD operations for crops

---
+++ crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/service/CropService.java
@@ -0,0 +1,13 @@
+package com.example.crop_monitoring_system.service;
+
+import com.example.crop_monitoring_system.dto.impl.CropDTO;
+
+import java.util.List;
+
+public interface CropService {
+    void saveCrop(CropDTO cropDTO);
+    void updateCrop(String cropCode, CropDTO cropDTO);
+    void deleteCrop(String cropCode);
+    CropDTO getSelectedCrop(String cropCode);
+    List<CropDTO> getAllCrops();
+}
