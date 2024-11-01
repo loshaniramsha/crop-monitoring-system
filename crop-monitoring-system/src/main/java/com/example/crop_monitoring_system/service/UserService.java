4e1aa43cf89cfcbeb8fd0b0659d624d0554be1da
loshaniramsha <loshanikottagoda@gmail.com>
2024-11-01 21:57:06 +0530

    Create UserService interface with CRUD methods

---
+++ crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/service/UserService.java
@@ -0,0 +1,13 @@
+package com.example.crop_monitoring_system.service;
+
+import com.example.crop_monitoring_system.dto.impl.UserDTO;
+
+import java.util.List;
+
+public interface UserService {
+    void saveUser(UserDTO userDTO);
+    void updateUser(String userId, UserDTO userDTO);
+    void deleteUser(String userId);
+    UserDTO getSelectedUser(String userId);
+    List<UserDTO> getAllUsers();
+}
