9480b1e0da79681d8268db3f7fa919baa84d2587
loshaniramsha <loshanikottagoda@gmail.com>
2024-11-01 21:45:17 +0530

    Create StaffService interface with CRUD operations for staff management

---
+++ crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/service/StaffService.java
@@ -0,0 +1,13 @@
+package com.example.crop_monitoring_system.service;
+
+import com.example.crop_monitoring_system.dto.impl.StaffDTO;
+
+import java.util.List;
+
+public interface StaffService {
+    void saveStaff(StaffDTO staffDTO);
+    void updateStaff(String staffId, StaffDTO staffDTO);
+    void deleteStaff(String staffId);
+    StaffDTO getSelectedStaff(String staffId);
+    List<StaffDTO> getAllStaffs();
+}
