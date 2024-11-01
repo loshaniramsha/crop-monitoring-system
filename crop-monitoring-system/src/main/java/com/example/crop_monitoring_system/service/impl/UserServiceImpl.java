d2f437fddefc1ef433cd3b39baf7a361d4bd4a95
loshaniramsha <loshanikottagoda@gmail.com>
2024-11-01 21:58:12 +0530

    Implement UserServiceImpl with CRUD operations (method bodies pending implementation)

---
+++ crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/service/impl/UserServiceImpl.java
@@ -0,0 +1,44 @@
+package com.example.crop_monitoring_system.service.impl;
+
+import com.example.crop_monitoring_system.dao.UserDAO;
+import com.example.crop_monitoring_system.dto.impl.UserDTO;
+import com.example.crop_monitoring_system.service.UserService;
+import com.example.crop_monitoring_system.utills.Mapping;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserDAO userDAO;
+    @Autowired
+    private Mapping mapping;
+    @Override
+    public void saveUser(UserDTO userDTO) {
+
+    }
+
+    @Override
+    public void updateUser(String userId, UserDTO userDTO) {
+
+    }
+
+    @Override
+    public void deleteUser(String userId) {
+
+    }
+
+    @Override
+    public UserDTO getSelectedUser(String userId) {
+        return null;
+    }
+
+    @Override
+    public List<UserDTO> getAllUsers() {
+        return List.of();
+    }
+}
