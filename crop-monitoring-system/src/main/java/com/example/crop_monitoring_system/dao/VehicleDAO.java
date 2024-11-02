604631ee5076739e823809a4adfb00bcabef817b
loshaniramsha <loshanikottagoda@gmail.com>
2024-11-02 20:45:11 +0530

    Add query to generate latest vehicle ID in VehicleEntity

--- crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/dao/VehicleDAO.java
+++ crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/dao/VehicleDAO.java
@@ -2,6 +2,9 @@ package com.example.crop_monitoring_system.dao;
 
 import com.example.crop_monitoring_system.entity.impl.VehicleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface VehicleDAO extends JpaRepository<VehicleEntity,String> {
+    @Query("select v.vehicleCode from VehicleEntity v order by v.vehicleCode desc limit 1")
+    String generateVehicleCode();
 }
