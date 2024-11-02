e0aa91fceeedbb4fd0d28b3f5f762d3c755df1db
loshaniramsha <loshanikottagoda@gmail.com>
2024-11-02 20:53:34 +0530

    Add saveVehicle method with vehicle code generation and persistence validation

--- crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/service/impl/VehicleServiceImpl.java
+++ crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/service/impl/VehicleServiceImpl.java
@@ -2,6 +2,8 @@ package com.example.crop_monitoring_system.service.impl;
 
 import com.example.crop_monitoring_system.dao.VehicleDAO;
 import com.example.crop_monitoring_system.dto.impl.VehicleDTO;
+import com.example.crop_monitoring_system.entity.impl.VehicleEntity;
+import com.example.crop_monitoring_system.exception.DataPersistException;
 import com.example.crop_monitoring_system.service.VehicleService;
 import com.example.crop_monitoring_system.utills.Mapping;
 import jakarta.transaction.Transactional;
@@ -19,6 +21,11 @@ public class VehicleServiceImpl implements VehicleService {
     private Mapping mapping;
     @Override
     public void saveVehicle(VehicleDTO vehicleDTO) {
+        vehicleDTO.setVehicleCode(generateVehicleCode());
+        VehicleEntity save=vehicleDAO.save(mapping.toVehicleEntity(vehicleDTO));
+        if (save==null){
+            throw new DataPersistException("Vehicle not saved");
+        }
 
     }
 
@@ -40,5 +47,17 @@ public class VehicleServiceImpl implements VehicleService {
     @Override
     public List<VehicleDTO> getAllVehicles() {
         return List.of();
+    }
+
+    @Override
+    public String generateVehicleCode() {
+        String MaxVehicleId=vehicleDAO.generateVehicleCode();
+        if (MaxVehicleId==null){
+            return "V00-001";
+
+        }
+        int newVehicleId=Integer.parseInt(MaxVehicleId.replace("V00-",""))+1;
+        return "V00-"+String.format("%03d",newVehicleId);
+
     }
 }
