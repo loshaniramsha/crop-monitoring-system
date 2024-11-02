d7ed7d64d852da19d4646c7afdf11aa8037b0e1b
loshaniramsha <loshanikottagoda@gmail.com>
2024-11-02 07:45:48 +0530

    Add regex patterns for ID validation in RegexProcess utility

---
+++ crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/utills/RegexProcess.java
@@ -0,0 +1,10 @@
+package com.example.crop_monitoring_system.utills;
+
+public class RegexProcess {
+    public static final String CROP_ID = "^C00-\\d{3}$";
+    public static final String EQUIPMENT_ID = "^E00-\\d{3}$";
+    public static final String FIELD_ID = "^F00-\\d{3}$";
+    public static final String LOG_ID = "^L00-\\d{3}$";
+    public static final String STAFF_ID = "^S00-\\d{3}$";
+    public static final String VEHICLE_ID = "^V00-\\d{3}$";
+}
