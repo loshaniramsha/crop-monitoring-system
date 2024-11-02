ed1bf6b836b018e80aa46f44c87ac44d7510e5dc
loshaniramsha <loshanikottagoda@gmail.com>
2024-11-02 08:43:48 +0530

    Implement UserService methods for CRUD operations with exception handling

--- crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/service/impl/UserServiceImpl.java
+++ crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/service/impl/UserServiceImpl.java
@@ -2,15 +2,19 @@ package com.example.crop_monitoring_system.service.impl;
 
 import com.example.crop_monitoring_system.dao.UserDAO;
 import com.example.crop_monitoring_system.dto.impl.UserDTO;
+import com.example.crop_monitoring_system.entity.impl.UserEntity;
+import com.example.crop_monitoring_system.exception.DataPersistException;
 import com.example.crop_monitoring_system.service.UserService;
 import com.example.crop_monitoring_system.utills.Mapping;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -24,31 +28,50 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void saveUser(UserDTO userDTO) {
+        UserEntity save=userDAO.save(mapping.toUserEntity(userDTO));
+        if (save==null){
+            throw new DataPersistException("User not saved");
+        }
 
     }
 
     @Override
     public void updateUser(String userId, UserDTO userDTO) {
+        Optional<UserEntity> searched=userDAO.findById(userId);
+        if (searched.isPresent()){
+            userDAO.save(mapping.toUserEntity(userDTO));
+        }
+        else {
+            throw new DataPersistException("User"+userId+"not found");
+        }
 
     }
 
     @Override
     public void deleteUser(String userId) {
+        if (userDAO.existsById(userId)){
+            userDAO.deleteById(userId);
+        }
+        else {
+            throw new DataPersistException("User"+userId+"not found");
+        }
 
     }
 
     @Override
     public UserDTO getSelectedUser(String userId) {
-        return null;
+        Optional<UserEntity> searched=userDAO.findById(userId);
+        if (searched.isPresent()){
+            return mapping.toUserDTO(searched.get());
+        }
+        else {
+            throw new DataPersistException("User"+userId+"not found");
+        }
     }
 
     @Override
     public List<UserDTO> getAllUsers() {
-        return List.of();
+        return mapping.toUserDTOList(userDAO.findAll());
     }
 
-    @Override
-    public String generateUserId() {
-        return "";
-    }
 }
