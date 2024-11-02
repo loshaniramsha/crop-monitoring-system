1c3cea07cb325db1236bf7e56cca1dde696d5573
loshaniramsha <loshanikottagoda@gmail.com>
2024-11-02 08:44:41 +0530

    Add role-based user saving logic in saveUser method

--- crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/controller/UserController.java
+++ crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/controller/UserController.java
@@ -1,4 +1,31 @@
 package com.example.crop_monitoring_system.controller;
 
+import com.example.crop_monitoring_system.dto.impl.UserDTO;
+import com.example.crop_monitoring_system.entity.Role;
+import com.example.crop_monitoring_system.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.Repository;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("api/v1/user")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> saveUser(@RequestBody UserDTO userDTO) {
+        if (userDTO.getRole().equals(Role.ADMIN)){
+            userService.saveUser(userDTO);
+            return ResponseEntity.ok("User saved successfully");
+        }else {
+            return ResponseEntity.ok("User not saved. Only Admin can save");
+        }
+
+    }
+
+
 }
