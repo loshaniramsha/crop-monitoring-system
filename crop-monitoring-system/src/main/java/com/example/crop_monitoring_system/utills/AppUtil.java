4e86537e2baf2da35e78883e71291a7b9bbdec75
loshaniramsha <loshanikottagoda@gmail.com>
2024-11-02 07:23:26 +0530

    remove Id generateCode

--- crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/utills/AppUtil.java
+++ crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/utills/AppUtil.java
@@ -3,26 +3,5 @@ package com.example.crop_monitoring_system.utills;
 import java.util.UUID;
 
 public class AppUtil {
-    public static String generateCropCode(){
-        return "CROP-"+ UUID.randomUUID();}
 
-    public static String generateEquipmentId(){
-        return "EQUIPMENT-"+ UUID.randomUUID();
-    }
-
-    public static String generateFieldCode(){
-        return "FIELD-"+ UUID.randomUUID();
-    }
-    public static String generateMonitoringLogCode(){
-        return "LOG-"+ UUID.randomUUID();
-    }
-    public static String generateStaffId(){
-        return "STAFF-"+ UUID.randomUUID();
-    }
-    public static String generateUserId(){
-        return "USER-"+ UUID.randomUUID();
-    }
-    public static String generateVehicleCode(){
-        return "VEHICLE-"+ UUID.randomUUID();
-    }
 }
