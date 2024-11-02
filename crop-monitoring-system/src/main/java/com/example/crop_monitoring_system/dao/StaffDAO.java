91d4f293c21f33e47ee3212995f08f90fb0566c3
loshaniramsha <loshanikottagoda@gmail.com>
2024-11-02 20:44:31 +0530

    Add query to generate latest staff ID in StaffEntity

--- crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/dao/StaffDAO.java
+++ crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/dao/StaffDAO.java
@@ -2,6 +2,9 @@ package com.example.crop_monitoring_system.dao;
 
 import com.example.crop_monitoring_system.entity.impl.StaffEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StaffDAO extends JpaRepository<StaffEntity,String> {
+    @Query("SELECT s.staffId FROM StaffEntity s ORDER BY s.staffId DESC LIMIT 1")
+    String generateStaffId();
 }
