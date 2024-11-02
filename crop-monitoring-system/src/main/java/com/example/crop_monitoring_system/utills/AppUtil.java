3c7aca81fc8ab0aa2054683e94058bcecf702bcb
loshaniramsha <loshanikottagoda@gmail.com>
2024-11-02 07:43:12 +0530

    Add image-to-Base64 conversion utility method

--- crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/utills/AppUtil.java
+++ crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/utills/AppUtil.java
@@ -1,7 +1,12 @@
 package com.example.crop_monitoring_system.utills;
 
+import java.util.Base64;
 import java.util.UUID;
 
 public class AppUtil {
+    public static String convertImageToBase64(byte[] image) {
+        return Base64.getEncoder().encodeToString(image);
+
+    }
 
 }
