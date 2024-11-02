ea0e3fa1083fd218ad1ece0acc2d549a3b00f653
loshaniramsha <loshanikottagoda@gmail.com>
2024-11-02 20:56:34 +0530

    Add VehicleController with saveVehicle endpoint for vehicle data handling

--- crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/controller/VehicleController.java
+++ crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/controller/VehicleController.java
@@ -1,4 +1,37 @@
 package com.example.crop_monitoring_system.controller;
 
+import com.example.crop_monitoring_system.dto.impl.VehicleDTO;
+import com.example.crop_monitoring_system.exception.DataPersistException;
+import com.example.crop_monitoring_system.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/vehicle")
 public class VehicleController {
+    @Autowired
+    private VehicleService vehicleService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveVehicle(@RequestBody VehicleDTO vehicleDTO){
+        try {
+            vehicleService.saveVehicle(vehicleDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }
+        catch (DataPersistException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
