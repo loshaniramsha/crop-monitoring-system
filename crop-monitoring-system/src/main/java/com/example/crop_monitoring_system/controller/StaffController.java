7daffca5e95ca22a3d9f67d8392a419497deb66c
loshaniramsha <loshanikottagoda@gmail.com>
2024-11-03 23:57:49 +0530

    Add staff saving

--- crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/controller/StaffController.java
+++ crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/controller/StaffController.java
@@ -1,4 +1,31 @@
 package com.example.crop_monitoring_system.controller;
 
+import com.example.crop_monitoring_system.dto.impl.StaffDTO;
+import com.example.crop_monitoring_system.exception.DataPersistException;
+import com.example.crop_monitoring_system.service.StaffService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/staff")
 public class StaffController {
+    private StaffService staffService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveStaff(@RequestBody StaffDTO staffDTO){
+        try {
+            staffService.saveStaff(staffDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }
+        catch (DataPersistException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
