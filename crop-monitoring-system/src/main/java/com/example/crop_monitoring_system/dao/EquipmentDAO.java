a1c779d2dfe3d94422fcf40dfece950406d28587
loshaniramsha <loshanikottagoda@gmail.com>
2024-11-04 07:03:06 +0530

    Fix generateEquipmentId query

--- crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/dao/EquipmentDAO.java
+++ crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/dao/EquipmentDAO.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface EquipmentDAO extends JpaRepository<EquipmentEntity,String> {
-    @Query("SELECT e FROM EquipmentEntity e WHERE e.equipmentId = ?1")
+    @Query("SELECT MAX(e.equipmentId) FROM EquipmentEntity e")
     String generateEquipmentId();
 }
