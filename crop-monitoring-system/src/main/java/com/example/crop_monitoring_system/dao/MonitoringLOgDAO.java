fe1f299ca167e4af3f00c482d338862a87724e9a
loshaniramsha <loshanikottagoda@gmail.com>
2024-11-04 23:39:51 +0530

    update Query id generate

--- crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/dao/MonitoringLOgDAO.java
+++ crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/dao/MonitoringLOgDAO.java
@@ -1,10 +1,16 @@
 package com.example.crop_monitoring_system.dao;
 
 import com.example.crop_monitoring_system.entity.impl.MonitoringLogEntity;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.awt.print.Pageable;
+import java.util.List;
+
 public interface MonitoringLOgDAO extends JpaRepository<MonitoringLogEntity,String> {
-    @Query("SELECT l FROM MonitoringLogEntity l ORDER BY l.logCode DESC LIMIT 1")
-    String genarateMonitoringLogId();
+    @Query("SELECT l.logCode FROM MonitoringLogEntity l ORDER BY l.logCode DESC")
+    String generateMonitoringLogId();
+
+
 }
