14a5082545d92095a66bed938b6e685435b86b72
loshaniramsha <loshanikottagoda@gmail.com>
2024-11-06 23:44:59 +0530

    add generateFieldCode query to retrieve latest field code

--- crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/dao/FieldDAO.java
+++ crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/dao/FieldDAO.java
@@ -2,6 +2,9 @@ package com.example.crop_monitoring_system.dao;
 
 import com.example.crop_monitoring_system.entity.impl.FieldEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FieldDAO extends JpaRepository<FieldEntity,String> {
+    @Query("select f.fieldCode from FieldEntity f order by f.fieldCode desc limit 1")
+    String generateFieldCode();
 }
