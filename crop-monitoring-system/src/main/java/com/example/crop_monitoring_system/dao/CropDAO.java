7d23bae496170a40f0020955be67ceac52b2e0c0
loshaniramsha <loshanikottagoda@gmail.com>
2024-11-06 23:36:06 +0530

    add query to retrieve the latest crop code in descending order

--- crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/dao/CropDAO.java
+++ crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/dao/CropDAO.java
@@ -2,6 +2,9 @@ package com.example.crop_monitoring_system.dao;
 
 import com.example.crop_monitoring_system.entity.impl.CropEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CropDAO extends JpaRepository<CropEntity,String> {
+    @Query("select c.cropCode from CropEntity c order by c.cropCode desc limit 1")
+    String generateCropCode();
 }
