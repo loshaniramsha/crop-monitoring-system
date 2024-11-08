310bc20877eb2a4966b704d1dbc7070da197a813
loshaniramsha <loshanikottagoda@gmail.com>
2024-11-08 07:31:28 +0530

    modify generateCropCode query to ensure correct limit syntax

--- crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/dao/MonitoringLOgDAO.java
+++ crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/dao/MonitoringLOgDAO.java
@@ -9,7 +9,7 @@ import java.awt.print.Pageable;
 import java.util.List;
 
 public interface MonitoringLOgDAO extends JpaRepository<MonitoringLogEntity,String> {
-    @Query("SELECT l.logCode FROM MonitoringLogEntity l ORDER BY l.logCode DESC")
+    @Query("SELECT l.logCode FROM MonitoringLogEntity l ORDER BY l.logCode DESC LIMIT 1")
     String generateMonitoringLogId();
 
 
