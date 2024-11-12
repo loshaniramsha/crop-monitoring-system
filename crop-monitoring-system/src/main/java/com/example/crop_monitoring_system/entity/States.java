3a5df3e9ad065dc981670d256c26463b816fdffa
loshaniramsha <loshanikottagoda@gmail.com>
2024-11-12 22:57:19 +0530

    Add toUpperCase method to States enum for consistent state handling

--- crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/entity/States.java
+++ crop-monitoring-system/src/main/java/com/example/crop_monitoring_system/entity/States.java
@@ -2,5 +2,9 @@ package com.example.crop_monitoring_system.entity;
 
 public enum States {
     AVAILABLE,
-    NOT_AVAILABLE
+    NOT_AVAILABLE;
+
+    public String toUpperCase() {
+        return name().toUpperCase();
+    }
 }
